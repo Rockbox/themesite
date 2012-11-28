736b7754e14b2234ea1eaaea3350465c61120588
Alex Mayer <magictrick4906@aim.com>
2012-11-27 23:27:32 -0500

    Update Database class to use prepared statements.
    Change query() function to use prepared statements.
    Remove unneeded functions, unused variables, and sqlite2 usage.
    Update all calls to query function from themesite class to use
    new prepared sql syntax.
    
    Change-Id: I1507b59fed2678a34bebca47b5c6aabb8e24b349

--- private/db.class.php
+++ private/db.class.php
@@ -21,14 +21,15 @@
  ****************************************************************************/
 
 /*
- * Simple DB class using sqlite and a bunch of assumptions.
+ * Simple DB class using SQLite3
  */
 class db {
     private $file;
     private $dh;
-    /* array of all table in the db */
-    /* if you add tables or entrys the will be automatically added */
-    /* WARNING dont move entry or you will loose data */
+    /*
+     * Array of all tables in the db
+     * This is used as a reference to the database hierarchy
+    */
     private $tables = array( 
             'checkwps' =>array('themeid'            => 'INTEGER' ,
                                'version_type'       => 'TEXT' ,
@@ -73,161 +74,73 @@ class db {
                                'type'               => 'TEXT'),
             'zipcontents'=>array('themeid'          => 'INTEGER',
                                 'filename'          => 'TEXT'));
-                                
+
     public function __construct($file) {
         $this->file = $file;
         /* open db */
         $this->dh = new SQLite3($file);
-// FIXME: database update currrently not working with SQLite3
-//
-//        if ($this->dh === false) {
-//            $this->error($err);
-//        }
-//        else {
-//            /* try to  create tables */
-//            /* wrap in transaction */
-//            $this->query("BEGIN TRANSACTION");
-//            /* create all tables if they dont exist */
-//            foreach($this->tables as $name => $table)
-//            {
-//                if($this->columntypes($name) == false)
-//                {
-//                    $sql = sprintf("CREATE TABLE %s(",$name);
-//                    foreach ($table as $entry => $type) {
-//                        $sql = sprintf("%s%s %s ,",$sql,$entry,$type);
-//                    } 
-//                    $sql = sprintf("%s)",chop($sql,','));
-//                    $this->query($sql);
-//                }
-//            }
-//            /* end transaction */
-//            $this->query("COMMIT");
-//            
-//            /* check if we need to add rows */
-//            $addColumns = array();
-//            foreach($this->tables as $name => $table)
-//            {
-//                $curtable = $this->columntypes($name);
-//                $diff = 0;
-//                foreach($table as $key => $entry)
-//                {
-//                    if(!array_key_exists($key,$curtable))
-//                    {
-//                        $diff = 1;
-//                    }
-//                }
-//                
-//                if($diff > 0) {
-//                    $addColumns[$name] = $table;
-//                }
-//            }
-//            
-//            /* add rows if needed */
-//            if(count($addColumns) > 0) {
-//                /* Sqlite2 doesnt support live column add, so backup, export, drop, import it */
-//                 /* backup db */
-//                $i = 0;
-//                do {
-//                    $backupname = sprintf("%s/themes-%s.db.bak",preconfig::privpath,"$i");
-//                    $i++;
-//                } while (file_exists($backupname));
-//                $cmd = sprintf("cp %s %s",$file,$backupname);
-//                system($cmd,$retval);
-//                if($retval != 0) {
-//                    $this->log(sprintf("Failed to backup DB for upgrade: %s",$backupname));   
-//                    die(sprintf("Failed to backup DB for upgrade: %s",$backupname));
-//                }    
-//                /* wrap in transaction */
-//                $this->query("BEGIN TRANSACTION");   
-//                foreach($addColumns as $name => $table)
-//                {
-//                    /* get complete table */ 
-//                    $sql = sprintf("SELECT RowID,* from %s",$name);
-//                    $tabledata = $this->query($sql);
-//                    $tabletypes = $this->columntypes($name);
-//                    /* drop tabe */
-//                    $sql = sprintf("DROP TABLE %s",$name); 
-//                    $this->query($sql);
-//                    /* create new table */
-//                    $sql = sprintf("CREATE TABLE %s(",$name);
-//                    foreach ($table as $entry => $type) {
-//                        $sql = sprintf("%s%s %s ,",$sql,$entry,$type);
-//                    } 
-//                    $sql = sprintf("%s)",chop($sql,','));
-//                    $this->query($sql);
-//                    /* fill in data */
-//                    while($tableentry = $tabledata->next()){
-//                        $sql = sprintf("INSERT INTO %s (rowid, ",$name);
-//                        foreach ($tabletypes as $entry => $type) {
-//                            $sql = sprintf("%s%s ,",$sql,db::quote($entry));
-//                        }
-//                        $sql = sprintf("%s) VALUES(%s, ",chop($sql,','),$tableentry['RowID']);
-//                        
-//                        foreach ($tabletypes as $entry => $type) {
-//                            $sql = sprintf("%s'%s' ,",$sql,db::quote($tableentry[$entry]));
-//                        }
-//                        $sql = sprintf("%s)",chop($sql,','));
-//                        
-//                        $this->query($sql);
-//                    }
-//                }
-//                /* end transaction */
-//                $this->query("COMMIT");
-//                $this->log(sprintf("Database upgraded. Backup is: %s",$backupname));    
-//            }
-//        }
     }
 
-    /* Log a message to the log table.   */
-    private function log($message) {
-        $sql = sprintf("INSERT INTO log (time, ip, admin, msg) VALUES (datetime('now'), 'self', 'selfupdate', '%s')",$message);
-        $this->query($sql);
-    }
-
-    public function query($sql) {
-        $res = $this->dh->query($sql);
-
-        if ($res === false) {
+    public function query($sql, $args = null){
+        //prepare the query
+        $stmt = $this->dh->prepare($sql);
+        if(is_array($args)){
+            //loop through each value that needs to be bound
+            foreach($args as $key => $val){
+                $stmt->bindValue($key, $val);
+            }
+        }
+        //run the query
+        $res = $stmt->execute();
+        //check for errors
+        if($res === false){
             $code = $this->dh->lastErrorCode();
-            $err = sprintf("%s (%d)", $this->dh->lastErrorMsg(), $code);
+            $err = sprintf('%s (%d)', $this->dh->lastErrorMsg(), $code);
+            $sql = $sql . ' ' . print_r($args, true);
             $this->error($err, $sql);
-        } else {
+        }else{
             return new result($res, $this->dh);
         }
     }
 
-    public function columntypes($table) {
-        return @sqlite_fetch_column_types($table, $this->dh, SQLITE_ASSOC);
+    /* Log a message to the log table. */
+    private function log($message) {
+        $sql = 'INSERT INTO log (time, ip, admin, msg) VALUES (datetime("now"), "self", "selfupdate", :msg)';
+        $args = array( ':msg' => $message );
+        $this->query($sql, $args);
     }
 
-    private function error($err, $sql = "") {
+    private function error($err, $sql = '') {
         /* 
          * Sometimes the error is empty, in which case the explanation can be
          * found like this
          */
-        if ($err == "") {
+        if ($err == '') {
             $code = $this->dh->lastErrorCode();
-            $err = sprintf("%s (%d)",
+            $err = sprintf('%s (%d)',
                 $this->dh->lastErrorMsg(),
                 $code
             );
         }
-        $msg = sprintf("<b>DB Error:</b> %s", $err);
-        if ($sql != "") {
+        $msg = sprintf('<b>DB Error:</b> %s', $err);
+        if ($sql != '') {
             $msg .= sprintf("<br />\n<b>SQL:</b> %s", $sql);
         }
         /* xxx: We'd probably want to log this rather than output it */
         die($msg);
     }
 
+    /*
+    * Used for cases where item cant be bound to a prepared statement
+    * Mostly used with "Order By" syntax or when a dynamic field name is needed
+    */
     public static function quote($input) {
         return SQLite3::escapeString($input);
     }
 }
 
 /*
- * Simple OO wrapper around the regular sqlite functions. Newer PHP versions
- * have something like this, but use this to avoid depending on that.
+ * Simple OO wrapper to make retrieving SQLite3 results easy.
  */
 class result {
     private $rh;
