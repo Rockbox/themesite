b4f09b766d9fd30d45d6fd81efaaa6f519c8448e
Solomon Peachy <pizza@shaftnet.org>
2020-04-13 17:28:49 -0400

    Parallelize checkwps builds, and track failures.

--- private/checkwps/current/update.sh
+++ private/checkwps/current/update.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 rockbox_dir="$HOME/rockbox_git_clone"
-unset ANDROID_SDK_PATH
-unset ANDROID_NDK_PATH
+
+export ANDROID_NDK_PATH="/home/rbbuild/x-tools/android-ndk-r10e"
 
 cd `dirname "$0"`
 target=`pwd`
 cd "${rockbox_dir}"
-git pull --rebase
-git checkout master
+#git pull --rebase
+#git checkout master
 ./tools/version.sh . > "${target}/VERSION"
 cd "tools/checkwps"
 ./cleanall.sh
-./buildall.sh
+./buildall.sh --jobs=`nproc`
 cp output/checkwps.* "${target}/"
+cp checkwps.failures "${target}/"
 cd "${target}"
 rm -f checkwps.c checkwps.h
